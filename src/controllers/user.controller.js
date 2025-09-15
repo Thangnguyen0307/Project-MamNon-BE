fd3504a8b8687894e65f2bc6fd09f9d9ba0b2c66
Le Nguyen Minh Kha <lenguyenminhkha1606@gmail.com>
2025-09-15 08:43:48 +0700

    Refactor getUserDetail function to remove console log and return user details based on userId from payload

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,10 +6,8 @@ export async function getUsers(req, res) {
 }
 
 export async function getUserDetail(req, res) {
-    console.log(req.payload);
     
-    // const user = await userService.getUserDetail(req.params.id);
-    return res.json(req.payload);
+    const user = await userService.getUserDetail(req.payload.userId);
     if (!user) return res.status(404).json({ message: 'User không tồn tại' });
     res.json(user);
 }
