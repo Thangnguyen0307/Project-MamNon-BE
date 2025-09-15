412d8982d166933a136d508eb8e42c6cb037cec1
Le Nguyen Minh Kha <lenguyenminhkha1606@gmail.com>
2025-09-15 14:56:14 +0700

    change method api webhook

--- src/routers/web-hook-zaloOARouter.js
+++ src/routers/web-hook-zaloOARouter.js
@@ -3,7 +3,7 @@ import { WebhookController } from '../controllers/webhook-zaloOA.controller.js';
 import path from 'path';
 const webhookZaloRouter = express.Router();
 
-webhookZaloRouter.get('/webhook', WebhookController.handle);
+webhookZaloRouter.post('/webhook', WebhookController.handle);
 
 webhookZaloRouter.get('/zalo_verifierNuAYTAlpU5Giyka8xkLO0oVNw5JBeJyFDJSs.html', (req, res) => {
     res.sendFile(
